ccb1225b1c32e7049d8c032dbebf19a7d728c648
Florian <12160@holbertonstudents.com>
2026-01-13 10:44:07 +0100

    dead copilot

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+for i in range(0, 10):
+    for j in range(0, 10):
+        if i == j:
+            continue
+        if j < i:
+            continue
+        if i == 8 and j == 9:
+            print("{}{}".format(i, j), end="\n")
+        else:
+            print("{}{}".format(i, j), end=", ")
