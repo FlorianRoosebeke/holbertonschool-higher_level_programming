2bdc76a363fcb62b7643ab7fbc631fa1e4926caf
Florian <12160@holbertonstudents.com>
2026-01-19 10:41:49 +0100

    fix: correct logic in no_c function to simplify character removal

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    result = ''.join([car for car in my_string if car != "c"] and [car for car in my_string if car != "C"])
+    result = ''.join([car for car in my_string if car != 'c' and car != 'C'])
     return result
