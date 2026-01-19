ddc89ab30120818843e89815fd194502b8e28ded
Florian <12160@holbertonstudents.com>
2026-01-19 12:58:37 +0100

    refactor: update divisible_by_2 function to return a list of even numbers

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list.copy()
-    for i in range(len(new_list)):
-        if new_list[i] % 2 == 0:
-            return True
-        else:
-            return False
+    result = my_list.copy()
+
+    for i in range(len(result)):
+        result[i] = (result[i] % 2 == 0)
+    return result
