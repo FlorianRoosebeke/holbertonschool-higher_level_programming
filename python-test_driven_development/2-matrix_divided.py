3afb5f5004d0c8060fc4341652bfcfa5986243dc
Florian <12160@holbertonstudents.com>
2026-01-22 13:26:55 +0100

    refactor: improve matrix validation checks in matrix_divided function

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,12 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-
+    
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+        
     new_matrix = []
     for ligne in matrix:
         new_ligne = []
