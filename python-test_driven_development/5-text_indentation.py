c88a1c700cfa34077b8dcc836f064f9a51baa049
Florian <12160@holbertonstudents.com>
2026-01-22 16:02:06 +0100

    fix: optimize text_indentation function by replacing for loop with while loop for better performance

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,14 +15,14 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for char in text:
-        i = 0
-        if char == "." or char == "?" or char == ":":
-            print(char)
+    i = 0
+    while i < len(text):
+        if text[i] in ".?:":
+            print(text[i])
             print()
             i += 1
             while i < len(text) and text[i] == " ":
                 i += 1
         else:
-            print(char, end='')
+            print(text[i], end='')
             i += 1
