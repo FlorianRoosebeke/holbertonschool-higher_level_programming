42201745cd8b29585f6f49325214ecdfaf82fe7e
Florian <12160@holbertonstudents.com>
2026-01-22 13:20:54 +0100

    feat: implement matrix_divided function to divide matrix elements by a given divisor

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix by div."""
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_matrix = []
+    for ligne in matrix:
+        new_ligne = []
+        for element in ligne:
+            if not isinstance(element, (int, float)):
+                raise TypeError("matrix must be a matrix of integers/floats")
+            new_ligne.append(round(element / div, 2))
+        new_matrix.append(new_ligne)
+
+    return new_matrix
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 3))
+print(matrix)
