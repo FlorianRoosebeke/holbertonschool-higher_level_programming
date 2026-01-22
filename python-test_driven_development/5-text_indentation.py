c4fec34a38f88215969aac05208bcd10c132a1b3
Florian <12160@holbertonstudents.com>
2026-01-22 15:37:02 +0100

    feat: implement text_indentation function to format text with new lines

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each '.', '?', and ':' character.
+
+    Args:
+        text (str): The text to be printed.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for char in text:
+        if char == "." or char == "?" or char == ":":
+            print(char)
+            print()
+        else:
+            print(char, end='')
