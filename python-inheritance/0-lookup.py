b8167856f9c6aabc5efb0d0cc5198cd386a82ecf
Florian <12160@holbertonstudents.com>
2026-02-02 11:51:42 +0100

    feat: implement MyList class with sorting functionality and add is_same_class function

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,2 +1,13 @@
-def lookup(obj):
-    return dir(obj)
+class MyList(list):
+    def print_sorted(self):
+        list_sort = self.sorted(list)
+        return list_sort
+my_list = MyList()
+my_list.append(1)
+my_list.append(4)
+my_list.append(2)
+my_list.append(3)
+my_list.append(5)
+print(my_list)
+my_list.print_sorted()
+print(my_list)
