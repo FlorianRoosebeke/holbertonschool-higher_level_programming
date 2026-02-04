cab881093fee101f523222b1e9e09ed1f0c37429
Florian <12160@holbertonstudents.com>
2026-02-04 11:35:47 +0100

    feat: add BaseGeometry, Rectangle, and Square classes with validation and area methods

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,2 +1,3 @@
+#!/usr/bin/python3
 def inherits_from(obj, a_class):
-     return isinstance(obj, a_class) and not type(obj) is a_class
+    return isinstance(obj, a_class) and not type(obj) is a_class
