7952e32015e2ed8787d96c91f631b6d2ec8ccaff
Florian <12160@holbertonstudents.com>
2026-02-12 11:11:23 +0100

    fix: add missing newline at end of file and improve docstring formatting in serialization functions

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+import json
+import pickle
+
 
 def serialize_and_save_to_file(data, filename):
-    
+    """Serialize and save data to a file using pickle.
+
+    Args:
+        data: The data to serialize.
+        filename: The filename to save to.
+    """
+    with open(filename, "wb") as f:
+        pickle.dump(data, f)
+
+
 def load_and_deserialize(filename):
+    """Load and deserialize data from a pickle file.
+
+    Args:
+        filename: The filename to load from.
+
+    Returns:
+        The deserialized data.
+    """
+    with open(filename, "rb") as f:
+        return pickle.load(f)
