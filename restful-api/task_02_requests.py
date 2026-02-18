7ec214bbe2b584d0f8adedcb09da2849db27d597
Florian <12160@holbertonstudents.com>
2026-02-18 10:41:22 +0100

    feat: add fieldnames and header to CSV writer in fetch_and_save_posts function

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -21,7 +21,8 @@ def fetch_and_save_posts():
 
     if response.status_code == 200:
         with open('posts.csv', 'w', newline='', encoding='utf-8') as f:
-            writer = csv.DictWriter(f)
+            writer = csv.DictWriter(f, fieldnames = ["id", "title", "body"])
+            writer.writeheader()
             writer.writerows(data)
 
 
