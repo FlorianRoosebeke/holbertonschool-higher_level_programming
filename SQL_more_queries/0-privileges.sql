3013a4a86d01c1b6f2538668150a3b5408131d70
Florian <12160@holbertonstudents.com>
2026-03-04 13:30:26 +0100

    fix: add missing semicolons in SQL scripts for proper syntax

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Script that lists all privileges of the MySQL users user_0d_1 and user_0d_2
 -- Usage: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
-SHOW GRANT for user_0d_1, user_0d_2 FROM localhost
+SHOW GRANT for user_0d_1, user_0d_2 FROM localhost;
