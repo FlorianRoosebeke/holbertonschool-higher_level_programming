273ed95ccaafc50b98fe3af0f89e8e058cc5d111
Florian <12160@holbertonstudents.com>
2026-03-05 14:36:46 +0100

    fix: update SQL query to join cities and states tables for accurate data retrieval

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -12,8 +12,10 @@ def main():
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
                          passwd=password, db=database)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM cities"
-                   " ORDER BY id ASC",)
+    cursor.execute("SELECT cities.id, cities.name, states.name"
+                   " FROM cities"
+                   " JOIN states ON cities.state_id = states.id"
+                   " ORDER BY cities.id ASC")
     states = cursor.fetchall()
     for state in states:
         print(state)
