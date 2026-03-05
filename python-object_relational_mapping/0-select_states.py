d387fdbde5d79f8283e6f90ab42f057d76dd9a96
Florian <12160@holbertonstudents.com>
2026-03-05 11:16:19 +0100

    fix: update SQL query to simplify ordering and improve code structure

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,26 +1,36 @@
 #!/usr/bin/python3
-"""List all states from the given MySQL database sorted by ascending id."""
+"""List all states from the database."""
 
 import MySQLdb
 import sys
 
 
-if __name__ == "__main__":
-    username = sys.argv[1]
+def main():
+    """Run the SQL query and print results."""
+    user = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=username,
+        user=user,
         passwd=password,
         db=database
-        charset="utf8"
     )
+
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
     states = cursor.fetchall()
+
     for state in states:
         print(state)
+
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    main()
