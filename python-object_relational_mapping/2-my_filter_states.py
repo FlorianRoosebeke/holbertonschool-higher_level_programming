f28aebefbf3dfe5ee1f847f75ca563ffa8d91346
Florian <12160@holbertonstudents.com>
2026-03-05 14:18:38 +0100

    fix: update docstring and simplify SQL query in state filtering script

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
-"""List all states from the database."""
-
+"""Display all values in the states table."""
 import MySQLdb
 import sys
 
 
 def main():
-    """Run the SQL query and print results."""
+    """Define the main function"""
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_searched = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database,
-    )
-
+    searched = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306, user=username,
+                         passwd=password, db=database)
     cursor = db.cursor()
-
-    cursor.execute("""
-        SELECT * FROM states
-        WHERE name LIKE %s
-        ORDER BY id ASC;
-        """, (state_searched,))
-
+    cursor.execute("SELECT * FROM states WHERE name = '{}'"
+                   " ORDER BY id ASC".format(searched))
     states = cursor.fetchall()
-
     for state in states:
         print(state)
-
     cursor.close()
     db.close()
 
