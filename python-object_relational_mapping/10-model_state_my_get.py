af64b3b80827cfcae7f463a6eff093f209e7ad05
Florian <12160@holbertonstudents.com>
2026-03-05 15:27:48 +0100

    refactor: update script to print the states.id of a State object matching the given name

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
-"""Print a State object from the database hbtn_0e_6_usa."""
+"""Print the states.id of a State object matching the given name."""
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 import sys
 
 
-def main():
-    """Define the main function"""
+if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
     search = sys.argv[4]
-    engine = create_engine('mysql+mysqldb://{}:{}'
-                           '@localhost:3306/{}'.format(username, password,
-                                                       database))
-    session = sessionmaker(bind=engine)
-    session = session()
-    states = session.query(State).filter(State.name == search).first()
-    if states:
-        print("{}".format(states.name))
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username, password, database))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.name == search).first()
+    if state:
+        print(state.id)
     else:
         print("Not found")
     session.close()
-
-
-if __name__ == "__main__":
-    main()
