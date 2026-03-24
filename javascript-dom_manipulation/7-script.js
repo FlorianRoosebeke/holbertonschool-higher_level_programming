ee7403a02c268181fd6c76e34617a392c06067f0
Florian <12160@holbertonstudents.com>
2026-03-24 14:02:19 +0100

    refactor: clean up comments and formatting in 7-script.js

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,15 +1,14 @@
 const list = document.querySelector("ul#list_movies");
 fetch("https://swapi-api.hbtn.io/api/films/?format=json")
-.then(response => response.json())
-.then(data => {
-    // Extraire la liste depuis JSON
-    const items = data.results;  // ou data.liste, data.items...
-    
-    // Vider + remplir
-    list.innerHTML = "";
-    items.forEach(item => {
-        const li = document.createElement("li");
-        li.textContent = item.title;
-        list.append(li);
-	});
-});
+    .then(response => response.json())
+    .then(data => {
+
+        const items = data.results;
+
+        list.innerHTML = "";
+        items.forEach(item => {
+            const li = document.createElement("li");
+            li.textContent = item.title;
+            list.append(li);
+        });
+    });
