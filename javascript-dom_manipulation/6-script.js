ec71c49e0157a41fd14dfd17ec9aa29402906c62
Florian <12160@holbertonstudents.com>
2026-03-24 13:52:32 +0100

    feat: fetch and display character name from SWAPI in the DOM

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,2 +1,6 @@
-document.getElementById('red_header').addEventListener('click', function ()
-{ document.querySelector('header').style.color = '#FF0000'; });
+fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+.then(response => response.json())
+.then(data => {
+	const nom = data.name
+	document.querySelector("#character").textContent = data.name;
+});
