d60d7d5ea015f539122699a0be208938907d7f39
Florian <12160@holbertonstudents.com>
2026-03-24 13:52:07 +0100

    feat: fetch and display movie titles from API in list

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,2 +1,15 @@
-document.getElementById('red_header').addEventListener('click', function ()
-{ document.querySelector('header').style.color = '#FF0000'; });
+const list = document.querySelector("ul#list_movies");
+fetch("https://swapi-api.hbtn.io/api/films/?format=json")
+.then(response => response.json())
+.then(data => {
+    // Extraire la liste depuis JSON
+    const items = data.results;  // ou data.liste, data.items...
+    
+    // Vider + remplir
+    list.innerHTML = "";
+    items.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item.title;
+        list.append(li);
+	});
+});
