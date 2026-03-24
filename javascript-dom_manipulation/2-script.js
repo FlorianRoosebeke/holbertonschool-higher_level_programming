97bce9cb1c07c5d1e43830cfbdfb06bb56960b13
Florian <12160@holbertonstudents.com>
2026-03-24 10:33:09 +0100

    feat: add 2-script.js to change header color on click

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,5 @@
+const maVariable = document.querySelector("#red_header");
+const header = document.querySelector("header");
+maVariable.addEventListener("click", function () {
+	header.classList.add('red')
+});
